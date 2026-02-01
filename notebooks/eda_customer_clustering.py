e064c3c357b1d5de9e3cc49fd4658b30ab8cc2ef
Aman <aman.choudhary1472@gmail.com>
2026-02-01 23:13:28 +0530

    Initial commit

---
+++ notebooks/eda_customer_clustering.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from pathlib import Path
+
+PROJECT_ROOT =  Path(__file__).resolve().parents[1]
+
+DATA_PATH = PROJECT_ROOT / "data" / "raw" / "Online Retail.xlsx"
+
+
+df = pd.read_excel(DATA_PATH)
+
+print(df.head(5))
+print("Dataset Shape : " ,df.shape)
